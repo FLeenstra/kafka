e55a05484977df6522b4d1a4ea368a5406e0e94a
Folkert Leenstra <mail@folkertleenstra.nl>
2024-01-30 21:43:41 +0100

    Seperated all kafka stuff into module
    kafkaconnector module is new as is custom folder for modules

--- readworker/src/main.py
+++ readworker/src/main.py
@@ -1,61 +1,16 @@
-from confluent_kafka import Consumer, KafkaException
+
 from icecream import ic
 import time
 import logging
-import configparser
+from custom.kafkaconnector.Kafkaconnector import Kafkaconnector
 
-def unravelList(kafaTopic):
-    returnList = []
-    for kv in kafaTopic.split(","):
-        returnList.append(str(kv))
-    logging.debug(ic(returnList))
-    return returnList
-
-#set the logging level #todo move to config
-logging.basicConfig(level=logging.DEBUG)
-
-config = configparser.ConfigParser()
-config.read('app/consumer.ini', encoding='utf-8')
-
-
-kafkaAddress = config['kafka']['address']
-kafkaTopic = config['kafka']['topic']
-kafkaTopic = unravelList(kafkaTopic)
-kafkaConsumerGroup = config['kafka']['consumergroup']
-
-
-conf = {'bootstrap.servers': kafkaAddress,
-        'group.id': kafkaConsumerGroup,
-        'auto.offset.reset': 'smallest'}
-
-def basic_consume_loop(consumer, topic, running):
-    try:
-        #consumer.subscribe(['testTopic'])
-        logging.debug(topic)
-        consumer.subscribe(topic)
-        logging.debug('subscribed to topic')
-
-        while running:
-            msg = consumer.poll(timeout=1.0)
-            if msg is None: continue
-
-            if msg.error():
-                raise KafkaException(msg.error())
-            else:
-                key = msg.key().decode('utf-8')
-                value = msg.value().decode('utf-8')
-                logging.debug(key)
-                logging.debug(value)
-    finally:
-        # Close down consumer to commit final offsets.
-        consumer.close()
-        logging.debug('closed kafka connection')
+loop = Kafkaconnector()
 
 eternity = True
-running = False
+time.sleep(20)
 while eternity:
     logging.info('doing a run')
-    consumer = Consumer(conf)
-    basic_consume_loop(consumer, kafkaTopic, running)
-    running = True
+    loop.connect()
+    logging.info('taking a 10 sec break')
     time.sleep(10)
+    
